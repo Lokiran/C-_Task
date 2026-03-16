1a6efd1b18242917008d6b3f137634ea7683c381
Lokiran <chadivelokiranreddy@gmail.com>
2026-03-16 17:04:26 +0530

    Add files via upload

---
+++ BonusTask.cs
@@ -0,0 +1,77 @@
+﻿//using System;
+//using System.Collections.Generic;
+
+//public class Student
+//{
+//    public string Name { get; set; } = "";
+//    public int Age { get; set; }
+//    public int Marks { get; set; }
+
+//    public string GetGrade() =>
+//        Marks switch
+//        {
+//            >= 90 => "A",
+//            >= 75 => "B",
+//            >= 50 => "C",
+//            _ => "Fail"
+//        };
+//}
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        Console.Write("Enter multiple students? (Y/N): ");
+//        string? input = Console.ReadLine();
+
+//        if (input?.ToUpper() == "Y")
+//            MultipleStudents();
+//        else
+//            SingleStudent();
+//    }
+
+//    static void SingleStudent() =>
+//        PrintStudent(ReadStudent());
+
+//    static void MultipleStudents()
+//    {
+//        List<Student> students = new();
+
+//        while (true)
+//        {
+//            students.Add(ReadStudent());
+
+//            Console.Write("Add another? (Y/N): ");
+//            if (Console.ReadLine()?.ToUpper() != "Y")
+//                break;
+//        }
+
+//        Console.WriteLine("\n--- Results ---");
+//        foreach (var s in students)
+//            PrintStudent(s);
+//    }
+
+//    static Student ReadStudent()
+//    {
+//        Student s = new Student();
+
+//        Console.Write("Name: ");
+//        s.Name = Console.ReadLine() ?? "";
+
+//        Console.Write("Age: ");
+//        s.Age = Convert.ToInt32(Console.ReadLine());
+
+//        Console.Write("Marks: ");
+//        s.Marks = Convert.ToInt32(Console.ReadLine());
+
+//        return s;
+//    }
+
+//    static void PrintStudent(Student s)
+//    {
+//        Console.WriteLine($"\nName: {s.Name}");
+//        Console.WriteLine($"Age: {s.Age}");
+//        Console.WriteLine($"Marks: {s.Marks}");
+//        Console.WriteLine($"Grade: {s.GetGrade()}");
+//    }
+//}
---
+++ Fibonacci.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace DemoApp
+//{
+//    internal class Task
+//    {
+//        public void Fibonacci(int count)
+//        {
+//            int a = 0, b = 1;
+
+//            Console.WriteLine("Fibonacci Series:");
+
+//            for (int i = 0; i < count; i++)
+//            {
+//                Console.Write(a + " ");
+//                int next = a + b;
+//                a = b;
+//                b = next;
+//            }
+
+//            Console.WriteLine();
+//        }
+//    }
+
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Task obj = new Task();
+
+//            Console.Write("Enter how many Fibonacci numbers to print: ");
+//            int count = Convert.ToInt32(Console.ReadLine());
+
+//            obj.Fibonacci(count);
+//        }
+//    }
+//}
