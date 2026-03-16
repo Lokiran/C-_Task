6ebd954a4839841ac002c95bcfbe260120a50270
Lokiran <chadivelokiranreddy@gmail.com>
2026-03-16 17:07:36 +0530

    Update Fibonacci.cs

--- Fibonacci.cs
+++ Fibonacci.cs
@@ -1,39 +1,39 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace DemoApp
-//{
-//    internal class Task
-//    {
-//        public void Fibonacci(int count)
-//        {
-//            int a = 0, b = 1;
+namespace DemoApp
+{
+    internal class Task
+    {
+        public void Fibonacci(int count)
+        {
+            int a = 0, b = 1;
 
-//            Console.WriteLine("Fibonacci Series:");
+            Console.WriteLine("Fibonacci Series:");
 
-//            for (int i = 0; i < count; i++)
-//            {
-//                Console.Write(a + " ");
-//                int next = a + b;
-//                a = b;
-//                b = next;
-//            }
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(a + " ");
+                int next = a + b;
+                a = b;
+                b = next;
+            }
 
-//            Console.WriteLine();
-//        }
-//    }
+            Console.WriteLine();
+        }
+    }
 
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Task obj = new Task();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Task obj = new Task();
 
-//            Console.Write("Enter how many Fibonacci numbers to print: ");
-//            int count = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter how many Fibonacci numbers to print: ");
+            int count = Convert.ToInt32(Console.ReadLine());
 
-//            obj.Fibonacci(count);
-//        }
-//    }
-//}
+            obj.Fibonacci(count);
+        }
+    }
+}
